8c421a812f4bd11aa29832cf097e4cf812e220f8
Spaceage1217 <dimeji.faluyi@gmail.com>
2017-05-24 20:28:20 -0500

    getting error with webpack

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -18,8 +18,17 @@ gulp.task('watch', function(){
   watch('./app/assets/styles/**/*.css', function(){
        gulp.start('cssInject');//jumps to styles function below
   });
+
+  watch('./app/assets/scripts/**/*.js',function(){
+    gulp.start('scriptsRefresh');
+  })
 });
 
 gulp.task('cssInject',['styles'],function(){
-  return gulp.src('./app/temp/styles/styles.css').pipe(browserSync.stream());
+  return gulp.src('./app/temp/styles/styles.css')
+  .pipe(browserSync.stream());
 });
+
+gulp.task('scriptsRefresh',['scripts'], function(){
+  browserSync.reload();
+})
